1269efc3a9dcd0938fc04179e5cf83ff3e873a65
李超 <371551077@qq.com>
2018-11-09 17:05:36 +0800

    添加导入钱包逻辑

--- UchainWallet/Wallet/Controllers/UchainImportWalletController.m
+++ UchainWallet/Wallet/Controllers/UchainImportWalletController.m
@@ -10,14 +10,64 @@
 
 @interface UchainImportWalletController ()
 
+@property (nonatomic, strong) UITableView *tableView;
+
 @end
 
 @implementation UchainImportWalletController
 
-- (void)viewDidLoad {
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    [self.navigationController lt_setBackgroundColor:[UIColor whiteColor]];
+}
+
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     
-    // Do any additional setup after loading the view.
+    self.title = SOLocalizedStringFromTable(@"Import Wallet", nil);
+    
+    UIButton *nextBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    nextBtn.frame = CGRectMake((kScreenWidth - 100) / 2, 240, 100, 30);
+    [nextBtn setTitle:@"导入" forState:UIControlStateNormal];
+    [nextBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    nextBtn.backgroundColor = [UchainUtil mainThemeColor];
+    [self.view addSubview:nextBtn];
+    [nextBtn addTarget:self action:@selector(btnClick) forControlEvents:UIControlEventTouchUpInside];
+}
+
+- (void)btnClick
+{
+    NSError *err = nil;
+    EthmobileWallet *wallet = EthmobileFromMnemonic(@"demise chalk clean merit until circle vendor broccoli fitness diet arch anger unlock immune smoke foil intact leopard panic brisk oxygen napkin trophy result", mnemonicEnglish, &err);
+    
+    if (err) {
+        [self showMessage:SOLocalizedStringFromTable(@"Import Wallet Failed", nil)];
+        return;
+    }
+    
+    NSError *keystoreErr = nil;
+    NSString *keystore = [wallet toKeyStore:@"123456" error:&keystoreErr];
+    if (keystoreErr) {
+        [self showMessage:[NSString stringWithFormat:@"%@: %@",SOLocalizedStringFromTable(@"Create Keystore Failed", nil) ,keystoreErr]];
+        return;
+    }
+    
+    NSString *address = wallet.address;
+    
+    [PDKeyChain save:KEYCHAIN_KEY(address) data:keystore];
+    [[ETHWalletManager shareManager] saveWallet:address name:@"Wallet"];
+    
+    [self showMessage:SOLocalizedStringFromTable(@"Import Wallet Success", nil)];
+    
+    if (self.didFinishImportSub) {
+        [self.didFinishImportSub sendNext:@""];
+    }
+    else {
+        [self.navigationController popViewControllerAnimated:YES];
+    }
 }
 
 /*
